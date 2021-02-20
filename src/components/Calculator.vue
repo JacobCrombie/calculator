f39c588f5231f697df14a393826cafbcc7f3a15e
Darbyblinko <jacobcrombie@live.com>
2021-02-20 11:30:13 -0700

    Intial commit for calculator build

---
+++ src/components/Calculator.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="calculator">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "calculator",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {},
+};
+</script>
+
+
+<style scoped>
+</style>
